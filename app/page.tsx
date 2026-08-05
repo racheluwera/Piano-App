a9adef89341eb75a50f2303ca8b15bdca48de961
racheluwera <racheluwera9@gmail.com>
2026-08-06 00:10:12 +0200

    First push

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Piano from "./Components/piano";
 
 export default function Home(){
-
   return(
     <main className="flex flex-col items-center justify-center min-h-screen">
     <h1 className="text-4xl font-bold mb-8">My piano</h1>
     <Piano/>
     </main>
   );
+
 }
